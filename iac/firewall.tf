2c0ebbe456902e35297312f23a9f9f70e8931ba0
Felipe Vilarinho <fvilarin@akamai.com>
2024-10-15 08:15:11 -0300

    Bug fixes.

--- iac/firewall.tf
+++ iac/firewall.tf
@@ -2,6 +2,8 @@ data "http" "myIp" {
   url = "https://ipinfo.io"
 }
 
+data "linode_nodebalancers" "default" {}
+
 resource "linode_firewall" "default" {
   label           = "${var.settings.cluster.label}-firewall"
   inbound_policy  = "DROP"
@@ -18,10 +20,23 @@ resource "linode_firewall" "default" {
     action   = "ACCEPT"
     label    = "allowed-ips"
     protocol = "TCP"
-    ports    = "22,80,443"
+    ports    = "80,443"
     ipv4     = concat(var.settings.cluster.allowedIps.ipv4, [ "${jsondecode(data.http.myIp.response_body).ip}/32" ])
     ipv6     = var.settings.cluster.allowedIps.ipv6
   }
 
   depends_on = [ data.http.myIp ]
+}
+
+resource "null_resource" "test" {
+  for_each = { for nodebalancer in data.linode_nodebalancers.default.nodebalancers : nodebalancer.id => nodebalancer }
+
+  triggers = {
+    always_run = timestamp()
+  }
+
+  provisioner "local-exec" {
+    quiet = true
+    command = "echo ${each.value.hostname}"
+  }
 }
