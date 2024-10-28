8ec9e96156ecd9e3ffc4d8268f2d21364fc5f1b3
Felipe Vilarinho <fvilarin@akamai.com>
2024-10-28 07:57:56 -0300

    Bug fixes.

--- bin/applyStackLabelsAndTags.sh
+++ bin/applyStackLabelsAndTags.sh
@@ -9,7 +9,7 @@ function checkDependencies() {
   fi
 
   if [ -z "$NAMESPACE" ]; then
-    echo "The cluster namespace file is not defined! Please define it first to continue!"
+    echo "The stack namespace file is not defined! Please define it first to continue!"
 
     exit 1
   fi
@@ -25,44 +25,51 @@ function checkDependencies() {
 function applyStackLabelsAndTags() {
   NODES=$($KUBECTL_CMD get nodes | grep lke | awk -F' ' '{print $1}')
 
+  echo "Applying labels and tags to nodes..."
+
+  TAGS_PARAMS=
+
+  for TAG in $TAGS
+  do
+    TAGS_PARAMS="$TAGS_PARAMS --tags \"$TAG\""
+  done
+
   for NODE in $NODES
   do
-    LABEL=$($KUBECTL_CMD get pods -o wide -n "$NAMESPACE" | grep minio | grep "$NODE" | awk -F' ' '{print $1}')
+    PODS=$($KUBECTL_CMD get pods -o wide -n "$NAMESPACE" | grep "$NODE" | awk -F' ' '{print $1}')
     NODE_IP=$($KUBECTL_CMD get node -o wide | grep "$NODE" | awk -F' ' '{print $7}')
     NODE_ID=$($LINODE_CLI_CMD linodes list --text | grep "$NODE_IP" | awk -F' ' '{print $1}')
     VOLUME_ID=$($LINODE_CLI_CMD volumes list --text | grep "$NODE_ID" | awk -F' ' '{print $1}')
     VOLUME=$($LINODE_CLI_CMD volumes list --text | grep "$NODE_ID" | awk -F' ' '{print $2}')
 
-    echo "Applying label $NODE to node $NODE_ID..."
-
     $LINODE_CLI_CMD linodes update --label "$NODE" "$NODE_ID" > /dev/null 2>&1
-
-    echo "Applying label $VOLUME to volume $VOLUME_ID..."
-
     $LINODE_CLI_CMD volumes update --label "$VOLUME" "$VOLUME_ID" > /dev/null 2>&1
 
-    echo "Applying tags $TAGS, $LABEL, $NAMESPACE to node $NODE_ID and volume $VOLUME_ID..."
+    ADDITIONAL_TAGS=
 
-    for TAG in $TAGS
+    for POD in $PODS
     do
-      $LINODE_CLI_CMD linodes update --tags "$TAG" --tags "$LABEL" --tags "$NAMESPACE" "$NODE_ID" > /dev/null 2>&1
-      $LINODE_CLI_CMD volumes update --tags "$TAG" --tags "$LABEL" --tags "$NAMESPACE" "$VOLUME_ID" > /dev/null 2>&1
+      ADDITIONAL_TAGS="$ADDITIONAL_TAGS --tags \"$POD\""
     done
+
+    ADDITIONAL_TAGS="$ADDITIONAL_TAGS --tags \"$NAMESPACE\""
+
+    eval "$LINODE_CLI_CMD linodes update $TAGS_PARAMS $ADDITIONAL_TAGS $NODE_ID > /dev/null 2>&1"
+    eval "$LINODE_CLI_CMD volumes update $TAGS_PARAMS $ADDITIONAL_TAGS $VOLUME_ID > /dev/null 2>&1"
   done
+
+  echo "Applying labels and tags to node balancers..."
 
   NODE_BALANCERS=$($KUBECTL_CMD get svc -n "$NAMESPACE" | grep LoadBalancer | awk -F' ' '{print $4}')
 
   for NODE_BALANCER in $NODE_BALANCERS
   do
-    LABEL=$($KUBECTL_CMD get svc -n "$NAMESPACE" | grep "$NODE_BALANCER" | awk -F' ' '{print $1}')
+    NODE_BALANCER_NAME=$($KUBECTL_CMD get svc -n "$NAMESPACE" | grep "$NODE_BALANCER" | awk -F' ' '{print $1}')
     NODE_BALANCER_ID=$($LINODE_CLI_CMD nodebalancers list --text | grep "$NODE_BALANCER" | awk -F' ' '{print $1}')
 
-    echo "Applying tags $TAGS, $LABEL, $NAMESPACE to node balancer $NODE_BALANCER_ID..."
+    ADDITIONAL_TAGS="--tags \"$NODE_BALANCER_NAME\" --tags \"$NAMESPACE\""
 
-    for TAG in $TAGS
-    do
-      $LINODE_CLI_CMD nodebalancers update --tags "$TAG" --tags "$LABEL" --tags "$NAMESPACE" "$NODE_BALANCER_ID" > /dev/null 2>&1
-    done
+    eval "$LINODE_CLI_CMD nodebalancers update $TAGS_PARAMS $ADDITIONAL_TAGS $NODE_BALANCER_ID > /dev/null 2>&1"
   done
 }
 
